1ae7610bba03ce1d3e81ad0dc5934cc753ead30f
Dave Clydesdae <daveclydesdale@gmail.com>
2022-12-26 11:29:04 +0100

    api config headers2

--- client/src/utils/api.tsx
+++ client/src/utils/api.tsx
@@ -1,12 +1,17 @@
 import axios from "axios";
+import {cookies} from "./cookies";
 
 type Method = 'get' | 'post';
 
+const auth_token = cookies.get('auth_token')
+axios.defaults.headers.common['Authorization'] = auth_token;
+axios.defaults.headers.common['x-auth-token'] = auth_token;
+
 const options = {
-    withCredentials: true,
-    credentials: 'include',
-    headers: { 'Content-Type': 'application/json' }  
-}
+  withCredentials: true,
+  credentials: "include",
+  headers: { "Content-Type": "application/json", Cookie: auth_token },
+};
 
 export const apiCall = async (method: Method, endpoint: string, payload?: any) => {
     const URL = process.env.REACT_APP_API_URL + endpoint;
