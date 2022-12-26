a9c64ddddfa23a03af7630a005bee43bf78e0e5e
Dave Clydesdae <daveclydesdale@gmail.com>
2022-12-26 13:41:14 +0100

    x-auth-token header2

--- client/src/utils/api.tsx
+++ client/src/utils/api.tsx
@@ -3,15 +3,12 @@ import {cookies} from "./cookies";
 
 type Method = 'get' | 'post';
 
-const auth_token = cookies.get('auth_token')
-axios.defaults.headers.common['x-auth-token'] = auth_token;
-
-const options = {
-  headers: { "Content-Type": "application/json" },
-};
-
 export const apiCall = async (method: Method, endpoint: string, payload?: any) => {
     const URL = process.env.REACT_APP_API_URL + endpoint;
+    const auth_token = cookies.get('auth_token')
+    const options = {
+      headers: { "Content-Type": "application/json", 'x-auth-token': auth_token },
+    };
     const response = await axios[method](URL, payload, options);
     return response
 }
